948d4947a591ccb97dc59a30f087576a85d03264
maggi <margarita.herazo.i@gmail.com>
2024-12-08 09:18:31 -0300

    changing imgs from the pages

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    
-    const navLinks = document.querySelectorAll("nav a");
   
-    
-    navLinks.forEach(link => {
-      link.addEventListener("click", function (event) {
-        event.preventDefault(); 
-        const page = this.getAttribute("data-page"); 
-  
-       
-        fetch(page)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error("Network response was not ok " + response.statusText);
-            }
-            return response.text();
-          })
-          .then(html => {
-            
-            document.getElementById("content").innerHTML = html;
-          })
-          .catch(error => {
-            console.error("Error loading page:", error);
-            document.getElementById("content").innerHTML = "<p>Error loading page. Please try again.</p>";
-          });
-      });
-    });
-  });
-  
+  initializeSlideshow();
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const slides = document.querySelectorAll(".slide");
-    let currentIndex = 0;
-  
-    const showNextSlide = () => {
-    
-      slides[currentIndex].classList.remove("active");
-  
-      
-      currentIndex = (currentIndex + 1) % slides.length;
-  
-   
-      slides[currentIndex].classList.add("active");
-    };
-  
-   
-    slides[currentIndex].classList.add("active");
-  
-    
-    setInterval(showNextSlide, 5000);
+  const navLinks = document.querySelectorAll("nav a");
+
+  navLinks.forEach(link => {
+      link.addEventListener("click", function (event) {
+          event.preventDefault();
+          const page = this.getAttribute("data-page");
+
+          fetch(page)
+              .then(response => {
+                  if (!response.ok) {
+                      throw new Error("Network response was not ok " + response.statusText);
+                  }
+                  return response.text();
+              })
+              .then(html => {
+                  document.getElementById("content").innerHTML = html;
+                  
+                  initializeSlideshow();
+              })
+              .catch(error => {
+                  console.error("Error loading page:", error);
+                  document.getElementById("content").innerHTML = "<p>Error loading page. Please try again.</p>";
+              });
+      });
   });
+});
+
+
+function initializeSlideshow() {
+  const slides = document.querySelectorAll(".slide");
+  let currentIndex = 0;
+
+  const showNextSlide = () => {
+      slides[currentIndex].classList.remove("active");
+
+      currentIndex = (currentIndex + 1) % slides.length;
+
+      slides[currentIndex].classList.add("active");
+  };
+
   
+  if (slides.length > 0) {
+      slides[currentIndex].classList.add("active");
+  }
+
+  
+  setInterval(showNextSlide, 4000);
+}
